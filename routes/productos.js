fe61637aa4b0714e846a6f8c44268de98efdc1fa
Sebastián Orozco <44331089+Psicopatologia@users.noreply.github.com>
2019-11-12 04:57:45 -0500

    Added header

--- routes/productos.js
+++ routes/productos.js
@@ -4,6 +4,7 @@ var router = express.Router();
 
 router.get('/', function(req, res, next) {
     models.Producto.findAll().then(producto => {
+        res.header('Access-Control-Allow-Origin', '*');
         res.send(producto);
       })
 });
