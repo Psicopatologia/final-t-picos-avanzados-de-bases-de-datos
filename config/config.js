02958990e5dfa1f3d1e7c1596ee318440645cbff
Sebastián Orozco <44331089+Psicopatologia@users.noreply.github.com>
2019-11-12 03:03:09 -0500

    modified for use in production

--- config/config.js
+++ config/config.js
@@ -21,6 +21,6 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOSTNAME,
     dialect: 'mysql',
-    use_env_variable: 'DATABASE_URL'
+    use_env_variable: 'mysql://jb8obgmvx9f2yxts:ojyzz01gxh2lkeac@lolyz0ok3stvj6f0.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/u0akmvm1urimibrv'
   }
 };
